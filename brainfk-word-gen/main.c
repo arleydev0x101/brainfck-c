d9ac19234df7fda33e3413cbdee2078611b245a5
arley-techie <arley0714@gmail.com>
2026-02-11 15:52:28 +0800

    Output only generator

--- brainfk-word-gen/main.c
+++ brainfk-word-gen/main.c
@@ -1,45 +1,70 @@
+#include <linux/limits.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void multiply(int, char);
 
+#define MAX_LIMIT 1000
+
 int main(int argc, char** argv) {
-	char input[9999];
+	char input[MAX_LIMIT] = {0};
 
 	if (argc == 1) {
 		printf("Enter an input: ");
-		fgets(input, 9999, stdin);
+		fgets(input, MAX_LIMIT, stdin);
 		input[strlen(input) - 1] = 0;
 	}
 
-	if (strlen(input))
-		for (int i = 1; i < argc; ++i){
-			for (int y = 0; y < strlen(argv[i]); ++y){
-				multiply(argv[i][y] % 10, '+');
-				if (y - 1 > 0){
-					if (argv[i][y - 1] != argv[i][y])
-						multiply(argv[i][y] % 10, '+');
-					else if (argv[i][y - 1] % argv[i][y] <= 9)
-						multiply(argv[i][y - 1] % argv[i][y], '+');
-				}
-				else if (argv[i][y] % 10 != 0){
-					printf(">");
-					multiply(argv[i][y] / 10, '+');
-					printf("[<");
-					multiply(10, '+');
-					printf(">-]<");
-				}
-				printf(".");
-			}
-			printf("\n");
-		}
+	for (int i = 1; i < argc; ++i) {
+		strcat(input, argv[i]);
+		input[strlen(input)] = ' ';
+	}
+	input[strlen(input) - 1] = 0;
 
-	else printf("%s [words]", argv[0]);
+	int tmp, t;
+
+	for (int i = 0; input[i]; ++i){
+		if (i){
+			tmp = abs(input[i] - input[i - 1]);
+			t = input[i] > input[i - 1];
+			if (tmp <= 10){
+				multiply(tmp, t ? '+' : '-');
+			}
+			else {
+				multiply(tmp % 10, t ? '+' : '-');
+				putchar('>');
+				multiply(tmp / 10, t ? '+': '-');
+				putchar('[');
+				putchar('<');
+				multiply(10,  '+');
+				putchar('>');
+				putchar('-');
+				putchar(']');
+				putchar('<');
+			}
+		}
+		else {
+			multiply(input[i] % 10, '+');
+			putchar('>');
+			multiply(input[i] / 10, '+');
+			putchar('[');
+			putchar('<');
+			multiply(10,  '+');
+			putchar('>');
+			putchar('-');
+			putchar(']');
+			putchar('<');
+		}
+		putchar('.');
+		putchar('\n');
+	}
 }
 // ++>+++++++[<++++++++++>-]<.
 
 void multiply(int n, char c){
 	for (int i = 0; i < n; ++i) {
-		printf("%c", c);
+		putchar(c);
 	}
 }
